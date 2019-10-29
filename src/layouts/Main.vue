0c7264befca2dff2c7b1b1a04fc3586201e3b81b
Alex Moreno <alex.almeida@snowmanlabs.com>
2019-10-29 16:14:40 -0300

    Adds Main Layout to system

---
+++ src/layouts/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="header">
+      <router-link :to="{ path: '/home' }" class="header__news-link">
+        News
+      </router-link>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" >
+  .header {
+    border-bottom: 1px solid #333;
+  }
+</style>
