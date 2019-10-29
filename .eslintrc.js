1e949b87baa87cee403697ed14aba3b68c39b633
Alex Moreno <alex.almeida@snowmanlabs.com>
2019-10-29 16:06:05 -0300

    Adiciona regra do prettier pra ele parar de chorar com ponto e virgula

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,13 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    semi: 0
+    semi: 0,
+    "prettier/prettier": [
+        "warn",
+        {
+          "semi": false,
+        }
+      ]
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
