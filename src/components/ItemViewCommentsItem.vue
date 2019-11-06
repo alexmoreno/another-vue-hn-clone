4ae7461b09faf6d6a1b207345a5595a534fc51e2
Alex Moreno <alex.almeida@snowmanlabs.com>
2019-11-06 15:50:12 -0300

    Adds "hours ago" to internal items

--- src/components/ItemViewCommentsItem.vue
+++ src/components/ItemViewCommentsItem.vue
@@ -1,12 +1,15 @@
 <template>
   <li class="comment__item">
-    <router-link
-      :to="{ path: `/user/${comment.by}` }"
-      v-html="comment.by"
-      tag="a"
-      class="comment__created-by"
-    >
-    </router-link>
+    <p class="comment__item-meta">
+      <router-link
+        :to="{ path: `/user/${comment.by}` }"
+        v-html="comment.by"
+        tag="a"
+        class="comment__created-by"
+      >
+      </router-link>&nbsp;
+      <span v-html="timeago.format(comment.time * 1000)" />
+    </p>
     <span v-html="comment.text" />
     <ItemViewComments
       v-if="comment.kids"
@@ -17,6 +20,7 @@
 </template>
 
 <script>
+import * as timeago from "timeago.js"
 import { mapState } from "vuex"
 export default {
   name: "ItemViewCommentsItem",
@@ -28,18 +32,23 @@ export default {
       comments: "items"
     })
   },
+  data() {
+    return {
+      timeago
+    }
+  },
   props: {
     comment: {
       type: Object
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss">
 .comment__created-by {
-  display: block;
   margin-bottom: 20px;
   text-decoration: none;
 }
+
 </style>
